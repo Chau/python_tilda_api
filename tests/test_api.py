4586c82927a0301691558ca954b552dbcfebe39f
Vera Chauzova <vchauzova@marfa.team>
2023-07-10 17:13:38 +0800

    Add tests for getting page info

--- tests/test_api.py
+++ tests/test_api.py
@@ -47,6 +47,7 @@ def project_info_request_success():
               }
             })
 
+
 @pytest.fixture
 def pages_list_success():
     return json.dumps({
@@ -80,6 +81,129 @@ def pages_list_success():
                 },
               ]
             })
+
+
+@pytest.fixture
+def page_info_success():
+    return json.dumps(
+        {
+            "status": "FOUND",
+            "result": {
+                "id": "1001",
+                "projectid": "1",
+                "title": "Page title",
+                "descr": "",
+                "img": "",
+                "featureimg": "",
+                "alias": "",
+                "date": "2014-05-16 14:45:53",
+                "sort": "80",
+                "published": "1419702868",
+                "html": "some html page code",
+                "filename": "page1001.html",
+                "js": [],
+                "css": []
+            }
+        }
+    )
+
+
+@pytest.fixture
+def page_full_success():
+    return json.dumps(
+        {
+            "status": "FOUND",
+            "result": {
+                "id": "1001",
+                "projectid": "1",
+                "title": "Page title",
+                "descr": "",
+                "img": "",
+                "featureimg": "",
+                "alias": "",
+                "date": "2014-05-16 14:45:53",
+                "sort": "80",
+                "published": "1419702868",
+                "html": "some html page code",
+                "filename": "page1001.html"
+            }
+        }
+    )
+
+
+@pytest.fixture
+def page_export_success():
+    return json.dumps(
+        {
+            "status": "FOUND",
+            "result": {
+                "id": "1001",
+                "projectid": "1",
+                "title": "Page title",
+                "descr": "",
+                "img": "",
+                "featureimg": "",
+                "alias": "",
+                "date": "2014-05-16 14:45:53",
+                "sort": "80",
+                "published": "1419702868",
+                "images": [
+                    {
+                        "from": "",
+                        "to": ""
+                    },
+                    {
+                        "from": "",
+                        "to": ""
+                    },
+                    {
+                        "from": "",
+                        "to": ""
+                    }
+                ],
+                "html": "body page html-code with local links to files",
+                "filename": "page1001.html"
+            }
+        }
+    )
+
+
+@pytest.fixture
+def page_full_export_success():
+    return json.dumps(
+                        {
+                            "status": "FOUND",
+                            "result": {
+                                "id": "1001",
+                                "projectid": "0",
+                                "title": "Page title",
+                                "descr": "",
+                                "img": "",
+                                "featureimg": "",
+                                "alias": "",
+                                "date": "2014-05-16 14:45:53",
+                                "sort": "80",
+                                "published": "1419702868",
+                                "images": [
+                                    {
+                                        "from": "",
+                                        "to": ""
+                                    },
+                                    {
+                                        "from": "",
+                                        "to": ""
+                                    },
+                                    {
+                                        "from": "",
+                                        "to": ""
+                                    }
+                                ],
+                                "html": "full page html-code with local links to files",
+                                "filename": "page1001.html"
+                            }
+                        }
+                    )
+
 
 @pytest.fixture
 def api_calling_fail():
@@ -151,6 +275,7 @@ def test_get_project_info(mocker, project_info_request_success):
                                 ]
                               }
 
+
 def test_get_project_info_fail(mocker, api_calling_fail):
     mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
         return_value=api_calling_fail
@@ -197,3 +322,175 @@ def test_get_pages_list(mocker, pages_list_success):
                               "filename": "page1002.html"
                             },
                           ]
+
+
+def test_get_pages_list_fail(mocker, api_calling_fail):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=api_calling_fail
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    with pytest.raises(TildaException):
+        tilda_api.get_pages_list(project_id=1)
+
+
+def test_get_page_success(mocker, page_info_success):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=page_info_success
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    page_info = tilda_api.get_page(page_id=1001)
+    assert page_info == {
+                            "id": "1001",
+                            "projectid": "1",
+                            "title": "Page title",
+                            "descr": "",
+                            "img": "",
+                            "featureimg": "",
+                            "alias": "",
+                            "date": "2014-05-16 14:45:53",
+                            "sort": "80",
+                            "published": "1419702868",
+                            "html": "some html page code",
+                            "filename": "page1001.html",
+                            "js": [],
+                            "css": []
+                        }
+
+
+def test_get_page_fail(mocker, api_calling_fail):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=api_calling_fail
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    with pytest.raises(TildaException):
+        tilda_api.get_page(page_id=1)
+
+
+def test_get_page_full_success(mocker, page_full_success):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=page_full_success
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    page_info = tilda_api.get_page_full(page_id=1001)
+    assert page_info == {
+                            "id": "1001",
+                            "projectid": "1",
+                            "title": "Page title",
+                            "descr": "",
+                            "img": "",
+                            "featureimg": "",
+                            "alias": "",
+                            "date": "2014-05-16 14:45:53",
+                            "sort": "80",
+                            "published": "1419702868",
+                            "html": "some html page code",
+                            "filename": "page1001.html"
+                        }
+
+
+def test_get_page_full_fail(mocker, api_calling_fail):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=api_calling_fail
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    with pytest.raises(TildaException):
+        tilda_api.get_page_full(page_id=1)
+
+
+def test_get_page_export_success(mocker, page_export_success):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=page_export_success
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    page_info = tilda_api.get_page_export(page_id=1001)
+    assert page_info == {
+                            "id": "1001",
+                            "projectid": "1",
+                            "title": "Page title",
+                            "descr": "",
+                            "img": "",
+                            "featureimg": "",
+                            "alias": "",
+                            "date": "2014-05-16 14:45:53",
+                            "sort": "80",
+                            "published": "1419702868",
+                            "images": [
+                                {
+                                    "from": "",
+                                    "to": ""
+                                },
+                                {
+                                    "from": "",
+                                    "to": ""
+                                },
+                                {
+                                    "from": "",
+                                    "to": ""
+                                }
+                            ],
+                            "html": "body page html-code with local links to files",
+                            "filename": "page1001.html"
+                    }
+
+
+def test_get_page_export_fail(mocker, api_calling_fail):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=api_calling_fail
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    with pytest.raises(TildaException):
+        tilda_api.get_page_export(page_id=1)
+
+
+def test_get_page_full_export_success(mocker, page_full_export_success):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=page_full_export_success
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    page_info = tilda_api.get_page_full_export(page_id=1001)
+    assert page_info == {
+                                "id": "1001",
+                                "projectid": "0",
+                                "title": "Page title",
+                                "descr": "",
+                                "img": "",
+                                "featureimg": "",
+                                "alias": "",
+                                "date": "2014-05-16 14:45:53",
+                                "sort": "80",
+                                "published": "1419702868",
+                                "images": [
+                                    {
+                                        "from": "",
+                                        "to": ""
+                                    },
+                                    {
+                                        "from": "",
+                                        "to": ""
+                                    },
+                                    {
+                                        "from": "",
+                                        "to": ""
+                                    }
+                                ],
+                                "html": "full page html-code with local links to files",
+                                "filename": "page1001.html"
+                            }
+
+
+def test_get_page_full_export_fail(mocker, api_calling_fail):
+    mocker.patch('api.urlopen').return_value.__enter__.return_value.read = mocker.Mock(
+        return_value=api_calling_fail
+    )
+    # calls api function
+    tilda_api = TildaApi()
+    with pytest.raises(TildaException):
+        tilda_api.get_page_full_export(page_id=1)
